13d99555e97cfb118b10e6015386df91a0c0818e
Daniel <dtkahn@gmail.com>
2018-10-10 21:52:16 -0400

    added react router and 2 component pages not actually related to this project just using this project as a workspace to practice react router setup

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Home = () => (
+    <React.Fragment>
+        <p>Home</p>
+        <Link to="/1">Page 1</Link>
+    </React.Fragment>
+);
+
+export default Home;
+
---
+++ src/components/Page1.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Page1 = () => (
+    <React.Fragment>
+        <p>Page 1</p>
+        <Link to="/">Home</Link>
+    </React.Fragment>
+);
+
+export default Page1;
+
